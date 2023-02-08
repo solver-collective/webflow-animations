114897692814c3020626e75cec94845fe78840ca
Nicholas Eli <nicholas@Nicholass-MacBook-Pro.local>
2023-02-08 14:32:06 -0800

    Stay at finale state

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const CSS = 'css';
 const selector = 'data-animation-type';
 const TYPES = [GREENSOCK, CSS];
 
-const create_style_tag = function (
-	element,
-	index,
-	duration = '1000ms',
-	delay = '0ms',
-	iteration = '1',
-	direction = 'forwards',
-	timing = 'linear'
-) {
+const create_style_tag = function (element, index, options) {
+	const { duration, delay, iteration, direction, timing } = options;
 	const style_tag = `
 		<style>
 			.animate_css_${index} {
+				-webkit-animation-name: animation-${index};
+			  -webkit-animation-duration: ${duration};
+			  -webkit-animation-delay: ${delay};
+			  -webkit-animation-iteration-count: ${iteration};
+			  -webkit-animation-direction: ${direction};
+			  -webkit-animation-timing-function: ${timing};
+			  -webkit-animation-fill-mode: forwards;
 				animation-name: animation-${index};
 			  animation-duration: ${duration};
 			  animation-delay: ${delay};
 			  animation-iteration-count: ${iteration};
 			  animation-direction: ${direction};
 			  animation-timing-function: ${timing};
+			  animation-fill-mode: forwards;
 			}
 		</style>
 	`;
@@ -38,13 +39,37 @@ const has_style_attribute = function (element, style_attribute) {
 const animate_css = function (element, index) {
 	const from = element.getAttribute('data-from');
 	const to = element.getAttribute('data-to');
+	const duration = element.getAttribute('data-duration');
+	const delay = element.getAttribute('data-delay');
+	const iteration = element.getAttribute('data-iteration');
+	const direction = element.getAttribute('data-direction');
+	const timing = element.getAttribute('data-timing');
+
 	if (!from || !to) return;
+
+	const options = {
+		duration: '1000ms',
+		delay: '0ms',
+		iteration: '1',
+		direction: 'normal',
+		timing: 'linear',
+	};
+
+	if (duration) options.duration = duration;
+	if (delay) options.delay = delay;
+	if (iteration) options.iteration = iteration;
+	if (direction) options.direction = direction;
+	if (timing) options.timing = timing;
 
 	element.classList.add('animate_css', 'animate_css_' + index);
 	if (!has_style_attribute(element, 'position')) element.style.position = 'relative';
 
 	const animation = `
 		<style>
+			@-webkit-keyframes animation-${index} {
+				from { ${from} }
+				to { ${to} }
+			}
 			@keyframes animation-${index} {
 				from { ${from} }
 				to { ${to} }
@@ -53,7 +78,7 @@ const animate_css = function (element, index) {
 	`;
 
 	document.body.insertAdjacentHTML('afterbegin', animation);
-	create_style_tag(element, index);
+	create_style_tag(element, index, options);
 };
 
 window.onload = function () {
